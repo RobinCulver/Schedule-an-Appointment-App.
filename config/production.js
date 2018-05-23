f6ecd8363cbb2fcdfb5122c857df9517a24a9257
Robin Culver <36706852+RobinCulver@users.noreply.github.com>
2018-05-22 20:02:25 -0400

    Initial commit

---
+++ config/production.js
@@ -0,0 +1,10 @@
+export const bucket = {
+  slug: process.env.COSMIC_BUCKET,
+  read_key: process.env.COSMIC_READ_KEY,
+  write_key: process.env.COSMIC_WRITE_KEY
+};
+export const twilio = {
+  auth: process.env.TWILLO_2e114108b47e0df0db897f7a910ecf1d,
+  sid: process.env.TWILLO_ACdb8fbd5bb0892d5b38f520df94864e5,
+  number: process.env.TWILIO_NUMBER
+};
